07a0e9d14d410d817d0dbb074f659c7a6d4f17c6
Marco <marcomarco85@hotmail.it>
2017-10-25 21:30:52 +0200

    Made an AbstractOhlcCandle

--- TradingFramework/src/candle/ohlc/OhlcCandle.java
+++ TradingFramework/src/candle/ohlc/OhlcCandle.java
@@ -1,37 +1,25 @@
 package candle.ohlc;
 
-public class OhlcCandle {
+import candle.ohlc.abstr.AbstractOhlcCandle;
 
-	private final double open;
-	private final double high;
-	private final double low;
-	private final double close;
+public class OhlcCandle extends AbstractOhlcCandle{
+
+
 	
 	public OhlcCandle(final double open, final double high, 
 			final double low, final double close) throws Exception{
 		
+		super(open,high,low,close);/*
 		this.open=open;
 		this.high=high;
 		this.low=low;
 		this.close=close;
-		checkValues();
+		checkValues();*/
 	}
 	
 	public OhlcCandle(final String open, final String high, 
 			final String low, final String close) throws Exception{
 	
 		this(Double.parseDouble(open),Double.parseDouble(high),Double.parseDouble(high),Double.parseDouble(close));
-	}
-	
-	private void checkValues() throws Exception{
-		
-		if(open<=0 || high<=0 || low<=0 || close<=0)
-			throw new Exception("Cannot handle negative values");
-		if(low>=high)
-			throw new Exception("Invalid values, low>=high. Low: "+low+" High: "+high);
-		if(!(open<=high && open>=low))
-			throw new Exception("Invalid values, !(open<=high && open>=low). Open: "+open+" High: "+high+" Low: "+low);
-		if(!(close<=high && close>=low))
-			throw new Exception("Invalid values, !(close<=high && close>=low). Close: "+close+" High: "+high+" Low: "+low);
 	}
 }
--- TradingFramework/src/candle/ohlc/temp/TempOhlcCandle.java
+++ TradingFramework/src/candle/ohlc/temp/TempOhlcCandle.java
@@ -1,31 +1,12 @@
 package candle.ohlc.temp;
 
-public final class TempOhlcCandle {
+import candle.ohlc.abstr.AbstractOhlcCandle;
 
-	private final double open;
-	private final double high;
-	private final double low;
-	private final double close;
+public final class TempOhlcCandle extends AbstractOhlcCandle{
 	
 	public TempOhlcCandle(final double open, final double high, 
 			final double low, final double close) throws Exception{
 		
-		this.open=open;
-		this.high=high;
-		this.low=low;
-		this.close=close;
-		checkValues();
-	}
-	
-	private void checkValues() throws Exception{
-		
-		if(open<=0 || high<=0 || low<=0 || close<=0)
-			throw new Exception("Cannot handle negative values");
-		if(low>=high)
-			throw new Exception("Invalid values, low>=high. Low: "+low+" High: "+high);
-		if(!(open<=high && open>=low))
-			throw new Exception("Invalid values, !(open<=high && open>=low). Open: "+open+" High: "+high+" Low: "+low);
-		if(!(close<=high && close>=low))
-			throw new Exception("Invalid values, !(close<=high && close>=low). Close: "+close+" High: "+high+" Low: "+low);
+		super(open,high,low,close);
 	}
 }
