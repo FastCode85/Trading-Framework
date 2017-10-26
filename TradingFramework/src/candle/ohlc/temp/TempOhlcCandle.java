ad8e9595d9733f830a5edab83339bcd0c39b8973
Marco <marcomarco85@hotmail.it>
2017-10-26 14:55:13 +0200

    TemoOhlcCandle now no longer extends AbstractOhlcCandle.

--- TradingFramework/src/candle/ohlc/temp/TempOhlcCandle.java
+++ TradingFramework/src/candle/ohlc/temp/TempOhlcCandle.java
@@ -1,12 +1,33 @@
 package candle.ohlc.temp;
 
-import candle.ohlc.abstr.AbstractOhlcCandle;
-
-public final class TempOhlcCandle extends AbstractOhlcCandle{
+public final class TempOhlcCandle{
+	
+	private final double open;
+	private final double high;
+	private final double low;
+	private final double close;
 	
 	public TempOhlcCandle(final double open, final double high, 
 			final double low, final double close) throws Exception{
 		
-		super(open,high,low,close);
+		this.open=open;
+		this.high=high;
+		this.low=low;
+		this.close=close;
+		checkValues();
 	}
+	
+	private final void checkValues() throws Exception{
+		
+		if(open<=0 || high<=0 || low<=0 || close<=0)
+			throw new Exception("Cannot handle negative values");
+		if(low>=high)
+			throw new Exception("Invalid values, low>=high. Low: "+low+" High: "+high);
+		if(!(open<=high && open>=low))
+			throw new Exception("Invalid values, !(open<=high && open>=low). Open: "+open+" High: "+high+" Low: "+low);
+		if(!(close<=high && close>=low))
+			throw new Exception("Invalid values, !(close<=high && close>=low). Close: "+close+" High: "+high+" Low: "+low);
+	}
+	
+	
 }
